b6384f9be84ab2762dc16d9a9df6c16a0274b239
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-15 04:37:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 def prime?(number)
   return false if number <=1
-  Math.sqrt(number).to_i.downto(2).each {|i| return false if number % i == 0}
+  Math.sqrt(number).to_i.downto(2).each do |i| 
+    return false if number % i == 0
+  end 
   true
 
 end
